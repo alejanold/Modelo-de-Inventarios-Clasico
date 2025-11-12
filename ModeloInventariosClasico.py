b8d7a7960082760b109209267465e91a6345d3ab
alejanold <iamaleortiz@gmail.com>
2025-11-12 01:59:03 -0600

    Se agrega el Proyecto

---
+++ ModeloInventariosClasico.py
@@ -0,0 +1,7 @@
+# Modelo de Inventarios EOQ Clásico
+# Autor: Ale Ortiz
+# Ingeniería en Sistemas Computacionales - 3er semestre
+
+import math
+
+print("==== MODELO DE INVENTARIOS EOQ CLÁSICO ====\n")
