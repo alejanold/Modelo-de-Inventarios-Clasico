4b655c3f517e264a87dcb9cb12175231e29d07ab
José Alejandro Canales Ortiz <iamaleortiz@gmail.com>
2025-11-12 22:59:15 -0600

    Primera parte (perdon por la tardanza)
    mas vale tarde que nunca, aquí pongo la primera parte para sacar el punto optimo del pedido

--- ModeloInventariosClasico.py
+++ ModeloInventariosClasico.py
@@ -5,3 +5,20 @@
 import math
 
 print("==== MODELO DE INVENTARIOS EOQ CLÁSICO ====\n")
+
+# Aquí pediré los datos para aplicar el modelo
+costo_pedido = float(input("Ingresa el costo de pedido (K): "))
+demanda = float(input("Ingresa la demanda del producto (D): "))
+costo_almacenamiento = float(input("Ingresa el costo de almacenamiento y proceso (h): "))
+dias_entrega = float(input("Ingrese en cuántos días recibes tu pedido (L): "))
+
+print("\n=== Aqui va el paso a paso ===")
+
+# Parte 1: punto optimo de pedido (y*)
+# Formula: y* = √(2 * D / h)
+# Segun yo aqui con este dato se indica la cantidad que conviene pedir :P
+print("\n1) Cálculo del punto óptimo de pedido (y*)")
+print(f"   y* = √(2 * {demanda} / {costo_almacenamiento})")
+y_optimo = math.sqrt((2 * demanda) / costo_almacenamiento)
+y_optimo_redondeado = round(y_optimo, 2)
+print(f"   y* = {y_optimo_redondeado}")
