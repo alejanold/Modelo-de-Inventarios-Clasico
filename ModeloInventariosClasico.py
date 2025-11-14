6aec5df626cbdb67ccd95c366b6ecb82a1bd3604
José Alejandro Canales Ortiz <iamaleortiz@gmail.com>
2025-11-13 19:16:23 -0600

    Ya casi; Costo total
    Ahora para que todo lo anterior tenga sentido pues debemos sacar nuestro costo total no? jaja

--- ModeloInventariosClasico.py
+++ ModeloInventariosClasico.py
@@ -55,3 +55,11 @@ punto_reorden = Le * demanda
 punto_reorden_redondeado = round(punto_reorden, 2)
 print(f"   Le * D = {punto_reorden_redondeado}")
 
+# Parte 6 Costo total del inventario (TCU)
+# Fórmula: TCU = (K / y* * D) + (h * (y* / 2))
+# En esta parte consideramos los pedidos y almacenamiento para sacar un total
+print("\n6) Cálculo del costo total del inventario (TCU)")
+print(f"   TCU = ({costo_pedido} / {y_optimo_redondeado} * {demanda}) + ({costo_almacenamiento} * ({y_optimo_redondeado} / 2))")
+TCU = (costo_pedido / y_optimo * demanda) + (costo_almacenamiento * (y_optimo / 2))
+TCU_redondeado = round(TCU, 2)
+print(f"   TCU = ${TCU_redondeado}")
